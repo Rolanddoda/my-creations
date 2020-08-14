4ad905ac19f57072f822a9efd38a53d3a31c47db
Roland Doda <rolanddoda2014@gmail.com>
2020-08-14 12:33:27 +0200

    Add chakra-ui/vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <c-theme-provider>
+    <c-reset />
     <router-view />
-  </div>
+  </c-theme-provider>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { CThemeProvider, CReset } from "@chakra-ui/vue";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: "App",
+  components: {
+    CThemeProvider,
+    CReset
   }
-}
-</style>
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Chakra from "@chakra-ui/vue";
+
+Vue.use(Chakra);
 
 Vue.config.productionTip = false;
 
