b8928fa6011569c8a3fcb91b084188069b436a31
Roland Doda <rolanddoda2014@gmail.com>
2020-08-14 16:56:29 +0200

    Remove chakra-ui and add vuetify

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./packages/router";
 import store from "./packages/vuex";
-import "./packages/chakra-ui";
+import vuetify from "./packages/vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
--- src/modules/home-page/HomePage.vue
+++ src/modules/home-page/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home-page">
+  <div class="home-page ove">
     Hooome pageee
   </div>
 </template>
