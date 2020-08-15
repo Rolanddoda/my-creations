0881ca0afd94d746c72c89f5c8a421dfa54144e6
Roland Doda <rolanddoda2014@gmail.com>
2020-08-15 09:42:52 +0200

    Add route cards in the homepage

--- src/modules/home-page/HomePage.vue
+++ src/modules/home-page/HomePage.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="home-page ove">
-    Hooome pageee
-  </div>
+  <v-container>
+    <v-row dense>
+      <v-col v-for="route of routes" :key="route.path" :cols="3">
+        <v-card shaped>
+          <v-card-title>
+            <div class="text-h6">{{ route.name }}</div>
+          </v-card-title>
+
+          <v-card-subtitle>Path: {{ route.path }}</v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn text :to="route.path" color="deep-purple accent-4">
+              Navigate
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import { routes } from "@/packages/router";
+
+export default {
+  created() {
+    this.routes = routes;
+  }
+};
+</script>
