5f03e488af28a6fd5ae1798f79ccbe04c7edce56
Roland Doda <rolanddoda2014@gmail.com>
2020-08-15 16:32:45 +0200

    Init GalleryPageTransition.vue

---
+++ src/modules/gallery-page-transition/GalleryPageTransition.vue
@@ -0,0 +1,56 @@
+<template>
+  <Grid gtr="auto 1fr" class="gallery-page-transition fill-height">
+    <div class="text-h3 text-center pa-10">Beautiful places</div>
+
+    <Grid :gtc="`repeat(${photos.length}, 30%)`" class="photos pa-5">
+      <div
+        class="photo"
+        v-for="photo of photos"
+        :key="photo.src"
+        :style="{ background: `url(${photo.src}) center / cover` }"
+      ></div>
+    </Grid>
+  </Grid>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    photos: [
+      {
+        src:
+          "https://cdn.statically.io/img/globalgrasshopper.com/wp-content/uploads/2020/03/most-beautiful-places-to-visit-in-Japan.jpg?f=auto"
+      },
+      {
+        src:
+          "https://www.studentuniverse.com/blog/wp-content/uploads/2014/04/Santorini-Greece.jpg"
+      },
+      {
+        src:
+          "https://cdn.statically.io/img/globalgrasshopper.com/wp-content/uploads/2013/11/Machu-Picchu-1.jpg?f=auto"
+      },
+      {
+        src:
+          "https://cdn.theculturetrip.com/wp-content/uploads/2018/08/perast.jpg"
+      },
+      {
+        src:
+          "https://i1.wp.com/adventurediary.co/wp-content/uploads/2019/12/aurora-borealis-1933239-min-scaled.jpg?fit=2560%2C1704&ssl=1"
+      }
+    ]
+  })
+};
+</script>
+
+<style lang="scss" scoped>
+@supports not (scrollbar-width: none) {
+  .photos::-webkit-scrollbar {
+    width: 0;
+  }
+}
+
+.photos {
+  overflow: auto;
+  scrollbar-width: none;
+}
+</style>
---
+++ src/modules/gallery-page-transition/routes.js
@@ -0,0 +1,8 @@
+export default {
+  path: "/gallery-page-transition",
+  name: "Gallery page transition",
+  component: () => import("./GalleryPageTransition"),
+  meta: {
+    icon: "mdi-page-layout-footer"
+  }
+};
