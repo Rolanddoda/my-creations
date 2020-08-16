d2839097d21d0a4c70c1ff852a527e2cfcfc3b8a
Roland Doda <rolanddoda2014@gmail.com>
2020-08-16 11:52:38 +0200

    Complete animation

--- src/modules/gallery-page-transition/GalleryPageTransition.vue
+++ src/modules/gallery-page-transition/GalleryPageTransition.vue
@@ -2,13 +2,14 @@
   <Grid gtr="auto 1fr" class="gallery-page-transition fill-height">
     <div class="text-h3 text-center pa-10">Beautiful places</div>
 
-    <div class="pa-10">
+    <div class="pa-15">
       <transition
         appear
-        @before-enter="selectedPhotoBeforeEnter"
-        @leave="selectedPhotoLeave"
+        @before-enter="beforeEnter"
+        @leave="leave"
         class="pa-5"
         :css="false"
+        mode="out-in"
       >
         <div
           v-if="selectedPhoto"
@@ -20,21 +21,21 @@
         >
           <SplitImage :cols="5" />
         </div>
-      </transition>
 
-      <Grid
-        v-if="!selectedPhoto"
-        :gtc="`repeat(${photos.length}, 30%)`"
-        class="photos fill-height pa-5"
-      >
-        <div
-          class="photo"
-          v-for="photo of photos"
-          :key="photo.src"
-          :style="{ background: `url(${photo.src}) center / cover` }"
-          @click="selectedPhoto = photo"
-        ></div>
-      </Grid>
+        <Grid
+          v-else
+          :gtc="`repeat(${photos.length}, 30%)`"
+          class="photos fill-height"
+        >
+          <div
+            class="photo"
+            v-for="photo of photos"
+            :key="photo.src"
+            :style="{ background: `url(${photo.src}) center / cover` }"
+            @click="selectedPhoto = photo"
+          ></div>
+        </Grid>
+      </transition>
     </div>
   </Grid>
 </template>
@@ -71,25 +72,62 @@ export default {
           "https://i1.wp.com/adventurediary.co/wp-content/uploads/2019/12/aurora-borealis-1933239-min-scaled.jpg?fit=2560%2C1704&ssl=1"
       }
     ],
-    selectedPhoto: {
-      src:
-        "https://cdn.statically.io/img/globalgrasshopper.com/wp-content/uploads/2020/03/most-beautiful-places-to-visit-in-Japan.jpg?f=auto"
-    }
+
+    selectedPhoto: null
   }),
 
   methods: {
-    selectedPhotoBeforeEnter(el) {
-      const item = el.querySelectorAll(".__s-item");
-      gsap.to(item, {
-        y: "100%",
-        delay: 0.2,
-        duration: 1,
+    beforeEnter(el) {
+      if (this.selectedPhoto) this.selectedPhotoBeforeEnter(el);
+      else this.photosBeforeEnter(el);
+    },
+
+    leave(el, done) {
+      if (this.selectedPhoto) this.photosLeave(el, done);
+      else this.selectedPhotoLeave(el, done);
+    },
+
+    getCommonGsapValues() {
+      return {
+        duration: 0.5,
         stagger: {
           from: "center",
           amount: 0.2
         }
+      };
+    },
+
+    photosBeforeEnter(el) {
+      const items = el.querySelectorAll(".photo");
+      gsap.fromTo(
+        items,
+        { y: "-100%" },
+        {
+          y: "0%",
+          ...this.getCommonGsapValues()
+        }
+      );
+    },
+
+    photosLeave(el, done) {
+      const items = el.querySelectorAll(".photo");
+      gsap.fromTo(
+        items,
+        { y: "0%" },
+        {
+          y: "-100%",
+          ...this.getCommonGsapValues(),
+          onComplete: done
+        }
+      );
+    },
+
+    selectedPhotoBeforeEnter(el) {
+      const item = el.querySelectorAll(".__s-item");
+      gsap.to(item, {
+        y: "100%",
+        ...this.getCommonGsapValues()
       });
-      console.log("before enter", el);
     },
 
     selectedPhotoLeave(el, done) {
@@ -97,11 +135,7 @@ export default {
       const tl = gsap.timeline({ onComplete: done });
       tl.to(item, {
         y: "0%",
-        duration: 1,
-        stagger: {
-          from: "center",
-          amount: 0.2
-        }
+        ...this.getCommonGsapValues()
       });
     }
   }
