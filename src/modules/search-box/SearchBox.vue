757dd7c3d0974fa497f19935c419f2d4229c5dc9
Roland Doda <rolanddoda2014@gmail.com>
2020-08-17 19:16:30 +0200

    Box should show circle at first

--- src/modules/search-box/SearchBox.vue
+++ src/modules/search-box/SearchBox.vue
@@ -1,10 +1,31 @@
 <template>
   <div class="search-box">
-    <div class="input-wrapper">
+    <div
+      class="input-wrapper"
+      ref="input"
+      data-input-opened="false"
+      @click="onClick"
+    >
       <input type="search" />
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onClick() {
+      const toggle = {
+        false: "true",
+        true: "false"
+      };
+
+      const inputState = this.$refs.input.getAttribute("data-input-opened");
+      this.$refs.input.setAttribute("data-input-opened", toggle[inputState]);
+    }
+  }
+};
+</script>
 
 <style lang="scss" scoped>
 .search-box {
@@ -20,32 +41,46 @@
 
   > .input-wrapper {
     position: relative;
+    cursor: pointer;
 
     > input {
       border-top: var(--border-width) solid;
       border-bottom: var(--border-width) solid;
       width: 0vw;
       height: var(--input-height);
+      outline: none;
     }
 
-    &:before {
+    &:before,
+    &:after {
       position: absolute;
       content: "";
       border: var(--border-width) solid;
-      //border-right: none;
-      //left: -1.25vw;
+      border-right: none;
       width: var(--input-height);
       height: var(--input-height);
-      border-radius: 50%;
-      //border-radius: 50% 0 0 50%;
+      border-radius: 50% 0 0 50%;
+      left: calc(-1 * var(--input-height) / 2);
+      clip-path: circle(50% at 50% 50%);
     }
 
     &:after {
-      //border-left: none;
+      border-left: none;
       border-right: var(--border-width) solid;
       left: unset;
-      //right: -1.25vw;
-      //border-radius: 0 50% 50% 0;
+      border-radius: 0 50% 50% 0;
+      right: calc(-1 * var(--input-height) / 2);
+    }
+
+    &[data-input-opened="true"] {
+      > input {
+        width: 20vw;
+      }
+
+      &:before,
+      &:after {
+        clip-path: unset;
+      }
     }
   }
 }
