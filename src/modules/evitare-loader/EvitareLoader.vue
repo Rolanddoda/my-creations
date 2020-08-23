2804dc8dc7275c5b5e39e4b2e69a429fb37f8115
Roland Doda <rolanddoda2014@gmail.com>
2020-08-23 09:53:13 +0200

    Split and remove extra code

--- src/modules/evitare-loader/EvitareLoader.vue
+++ src/modules/evitare-loader/EvitareLoader.vue
@@ -8,7 +8,7 @@
     <button @click="pause">Pause</button>
     <Grid gaf="column" class="box-wrapper">
       <div class="box" style="--index: 0"></div>
-      <div class="box" v-viz style="--index: 1;"></div>
+      <div class="box" style="--index: 1;"></div>
       <div class="box" style="--index: 2"></div>
       <div class="box" style="--index: 3"></div>
       <div class="box" style="--index: 4"></div>
@@ -56,18 +56,21 @@ export default {
       else tl.pause();
       pause = !pause;
     },
+
     mounted() {
       const [box0, ...boxes] = this.$el.querySelectorAll(".box");
-      const { duration, boxSize, gap } = this.getPayload();
+      const payload = this.getPayload();
 
-      tl = gsap.timeline({ yoyo: true, repeat: -1, repeatDelay: 0.3 });
-      tl.to(
-        box0,
-        { left: boxSize * 4 + 4 * gap, duration, ease: "none" },
-        "start"
-      );
+      tl = gsap.timeline({
+        yoyo: true,
+        repeat: -1,
+        repeatDelay: 0.3
+      });
+
+      this.firstBoxFromLeftToRight(box0, payload);
       boxes.forEach((box, index) => {
-        this.boxFirstAnim(tl, box, index + 1);
+        this.rotateBox(box, index + 1, payload);
+        this.squashBox(box, payload);
       });
     },
 
@@ -77,17 +80,26 @@ export default {
         getComputedStyle(this.$el).getPropertyValue("--box-size")
       );
       const gap = boxSize / 2;
+      const boxDuration = duration / 4;
+
       return {
         boxSize,
         duration,
-        gap
+        gap,
+        boxDuration
       };
     },
 
-    boxFirstAnim(tl, box, index) {
-      const { boxSize, duration, gap } = this.getPayload();
+    firstBoxFromLeftToRight(box, { duration, gap, boxSize }) {
+      tl.to(
+        box,
+        { left: boxSize * 4 + 4 * gap, duration, ease: "none" },
+        "start"
+      );
+    },
+
+    rotateBox(box, index, { boxSize, boxDuration, duration, gap }) {
       const quarterBoxSize = boxSize / 4;
-      const boxAnimDuration = duration / 4;
 
       tl.to(
         box,
@@ -97,8 +109,10 @@ export default {
           duration: duration / 1.5,
           ease: "none"
         },
-        `${((index - 1) * boxAnimDuration) / 2}`
+        `${((index - 1) * boxDuration) / 2}`
       );
+
+      /* Remove rotation and change transform-origin */
       tl.to(
         box,
         {
@@ -110,13 +124,15 @@ export default {
         },
         ">"
       );
+    },
 
+    squashBox(box, { boxDuration }) {
       tl.to(
         box,
         {
           scaleY: 0.5,
           scaleX: 1.5,
-          duration: boxAnimDuration / 3,
+          duration: boxDuration / 3,
           ease: "none"
         },
         ">"
@@ -127,71 +143,11 @@ export default {
         {
           scaleY: 1,
           scaleX: 1,
-          duration: boxAnimDuration / 3,
+          duration: boxDuration / 3,
           ease: "none"
         },
         ">"
       );
-    },
-
-    boxSecondAnim(tl, box, index) {
-      const { boxSize, duration, gap } = this.getPayload();
-
-      tl.to(
-        box,
-        {
-          left: (boxSize + gap / 2) * index,
-          rotation: -90,
-          top: -(boxSize + gap),
-          duration: duration / 12,
-          ease: "none"
-        },
-        ">"
-      );
-    },
-
-    boxThirdAnim(tl, box, index) {
-      const { boxSize, gap, duration } = this.getPayload();
-
-      tl.to(
-        box,
-        {
-          left: (boxSize + gap) * (index - 1),
-          rotation: -180,
-          top: 0,
-          duration: duration / 20,
-          ease: "none"
-        },
-        ">"
-      );
-    },
-
-    boxFourthAnim(tl, box) {
-      const { duration } = this.getPayload();
-
-      // tl.set(box, { scaleY: 0 });
-
-      // tl.to(
-      //   box,
-      //   {
-      //     rotation: -180,
-      //     scaleY: -0.5,
-      //     transformOrigin: "bottom",
-      //     duration: duration / 12,
-      //     ease: "none"
-      //   },
-      //   ">"
-      // );
-      // tl.to(
-      //   box,
-      //   {
-      //     scaleY: 1,
-      //     transformOrigin: "center",
-      //     duration: duration / 5,
-      //     ease: "none"
-      //   },
-      //   ">"
-      // );
     }
   }
 };
