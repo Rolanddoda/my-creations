b5f4296e7b46a791feed15cbc263e5655c855cc0
Roland Doda <rolanddoda2014@gmail.com>
2020-08-30 19:39:45 +0200

    Init form stepper

---
+++ src/form-stepper/FormStepper.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="form-stepper">
+    Form stepper
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/form-stepper/routes.js
@@ -0,0 +1,8 @@
+export default {
+  path: "form-stepper",
+  name: "Form Stepper",
+  component: () => import("./FormStepper"),
+  meta: {
+    icon: "mdi-format-list-checkbox"
+  }
+};
