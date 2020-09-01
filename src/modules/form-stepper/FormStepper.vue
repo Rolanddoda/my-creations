b4fa042bdb55e38b6a53314b0d285b1227f21a18
Roland Doda <rolanddoda2014@gmail.com>
2020-09-01 11:21:55 +0200

    Add transitions

--- src/modules/form-stepper/FormStepper.vue
+++ src/modules/form-stepper/FormStepper.vue
@@ -37,6 +37,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/shared/styles/variables";
+
 .form-stepper {
   --active-color: #3187f2;
   --inactive-color: #aaa;
@@ -62,22 +64,30 @@ export default {
     z-index: 1;
     cursor: pointer;
     user-select: none;
+    transition: background-color 0.1s $easing 0.25s;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      width: 130%;
+      height: 130%;
+      margin: -15%;
+      border: solid 2px var(--inactive-color);
+      border-radius: 50%;
+      transform: scale(0.8);
+      transition: transform 0.1s $easing 0.25s;
+    }
 
     &[data-active] {
       background: var(--active-color);
 
       &::before {
-        content: "";
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        width: 130%;
-        height: 130%;
-        margin: -15%;
-        border: solid 2px var(--active-color);
-        border-radius: 50%;
+        transform: scale(1);
+        border-color: var(--active-color);
       }
     }
   }
@@ -98,6 +108,7 @@ export default {
       background: var(--active-color);
       transform: scaleY(var(--line-completion));
       transform-origin: top;
+      transition: transform 0.25s cubic-bezier(0.36, 0, 0.66, -0.56);
     }
   }
 }
