ec58ad8e6a9b48fb7156f0c0cc9fbf63921ec724
Roland Doda <rolanddoda2014@gmail.com>
2020-10-11 12:10:49 +0200

    GlSlider on its own file + disable color split wave effect

--- src/modules/my-stats/MyStats.vue
+++ src/modules/my-stats/MyStats.vue
@@ -133,16 +133,14 @@
         else y=((x*2.)-vUv.x);
         x=smoothstep(0.,1.,y);
 
-        float w=vWave;
-
-        float r1=texture2D(uCurrTex,texUv+w*.04).r;
-        float g1=texture2D(uCurrTex,texUv+w*.01).g;
-        float b1=texture2D(uCurrTex,texUv+w*-.03).b;
+        float r1=texture2D(uCurrTex,texUv*.04).r;
+        float g1=texture2D(uCurrTex,texUv*.01).g;
+        float b1=texture2D(uCurrTex,texUv*-.03).b;
         vec3 tex1=vec3(r1,g1,b1);
 
-        float r2=texture2D(uNextTex,texUv+w*.04).r;
-        float g2=texture2D(uNextTex,texUv+w*.01).g;
-        float b2=texture2D(uNextTex,texUv+w*-.03).b;
+        float r2=texture2D(uNextTex,texUv*.04).r;
+        float g2=texture2D(uNextTex,texUv*.01).g;
+        float b2=texture2D(uNextTex,texUv*-.03).b;
         vec3 tex2=vec3(r2,g2,b2);
 
         float scaleUp=(.4+.6*(1.-uProg));
@@ -164,277 +162,17 @@
 </template>
 
 <script>
-import * as THREE from "three";
 import gsap from "gsap";
 import { Mouse } from "./webGl/Mouse";
-import { initWebGl } from "./webGl/webGl";
+import { GlSlider } from "./webGl/GlSlider";
 
 export default {
   mounted() {
-    let nMouse = new THREE.Vector2();
-    window.addEventListener("mousemove", event => {
-      event.preventDefault();
-      nMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-      nMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    });
-
-    let mouseOver = false,
-      mouseDown = false;
-
-    const vertexShader = document.getElementById("vertexShader").innerHTML;
-    const fragmentShader = document.getElementById("fragmentShader").innerHTML;
-
-    const planeGeometry = new THREE.PlaneBufferGeometry(1, 1, 32, 32);
-    const planeMaterial = new THREE.ShaderMaterial({
-      vertexShader,
-      fragmentShader
-    });
-
     // -------- UTILITY FUNCTIONS & CLASSES [START] --------
 
     const lerp = (a, b, n) => (1 - n) * a + n * b;
 
     // -------- UTILITY FUNCTIONS & CLASSES [END] --------
-
-    // -------- GL CLASSES [START] --------
-
-    const { scene, camera } = initWebGl();
-
-    class GlObject extends THREE.Object3D {
-      init(el) {
-        this.el = el;
-        this.resize();
-      }
-
-      resize() {
-        this.setBounds();
-      }
-
-      setBounds() {
-        this.rect = this.el.getBoundingClientRect();
-
-        this.bounds = {
-          left: this.rect.left,
-          top: this.rect.top + window.scrollY,
-          width: this.rect.width,
-          height: this.rect.height
-        };
-
-        this.updateSize();
-        this.updatePosition();
-      }
-
-      updateSize() {
-        this.camUnit = this.calculateUnitSize(
-          camera.position.z - this.position.z
-        );
-
-        const x = this.bounds.width / window.innerWidth;
-        const y = this.bounds.height / window.innerHeight;
-
-        if (!x || !y) return;
-
-        this.scale.x = this.camUnit.width * x;
-        this.scale.y = this.camUnit.height * y;
-      }
-
-      calculateUnitSize(distance = this.position.z) {
-        const vFov = (camera.fov * Math.PI) / 180;
-        const height = 2 * Math.tan(vFov / 2) * distance;
-        const width = height * camera.aspect;
-
-        return { width, height };
-      }
-
-      updateY(y = 0) {
-        const { top, height } = this.bounds;
-
-        this.position.y = this.camUnit.height / 2 - this.scale.y / 2;
-        this.position.y -=
-          ((top - y) / window.innerHeight) * this.camUnit.height;
-
-        this.progress = gsap.utils.clamp(
-          0,
-          1,
-          1 - (-y + top + height) / (window.innerHeight + height)
-        );
-      }
-
-      updateX(x = 0) {
-        const { left } = this.bounds;
-
-        this.position.x = -(this.camUnit.width / 2) + this.scale.x / 2;
-        this.position.x +=
-          ((left + x) / window.innerWidth) * this.camUnit.width;
-      }
-
-      updatePosition(y) {
-        this.updateY(y);
-        this.updateX(0);
-      }
-    }
-
-    class GlSlider extends GlObject {
-      init(el) {
-        super.init(el);
-
-        this.geometry = planeGeometry;
-        this.material = planeMaterial.clone();
-
-        this.material.uniforms = {
-          uCurrTex: { value: 0 },
-          uNextTex: { value: 0 },
-          uTime: { value: 0 },
-          uProg: { value: 0 },
-          uAmplitude: { value: 0 },
-          uProgDirection: { value: 0 },
-          uMeshSize: { value: [this.rect.width, this.rect.height] },
-          uImageSize: { value: [0, 0] },
-          uMousePos: { value: [0, 0] },
-          uMouseOverAmp: { value: 0 },
-          uAnimating: { value: false },
-          uRadius: { value: 0.08 },
-          uTranslating: { value: true }
-        };
-
-        this.imageScale = 1;
-
-        this.textures = [];
-
-        this.raycaster = new THREE.Raycaster();
-        this.mouse = new THREE.Vector2();
-        this.mouseLerpAmount = 0.1;
-
-        this.state = {
-          animating: false,
-          current: 0
-        };
-
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-        this.add(this.mesh);
-
-        scene.add(this);
-
-        this.loadTextures();
-        this.addEvents();
-      }
-
-      loadTextures() {
-        const manager = new THREE.LoadingManager(() => {
-          this.material.uniforms.uCurrTex.value = this.textures[0];
-        });
-        const loader = new THREE.TextureLoader(manager);
-        const imgs = [...this.el.querySelectorAll("img")];
-
-        imgs.forEach(img => {
-          loader.load(img.src, texture => {
-            texture.minFilter = THREE.LinearFilter;
-            texture.generateMipmaps = false;
-
-            this.material.uniforms.uImageSize.value = [
-              img.naturalWidth,
-              img.naturalHeight
-            ];
-            this.textures.push(texture);
-          });
-        });
-      }
-
-      updateTime(time) {
-        this.material.uniforms.uTime.value = time;
-        this.run();
-      }
-
-      addEvents() {
-        this.el.addEventListener("mouseenter", () => (mouseOver = true));
-        this.el.addEventListener("mouseleave", () => (mouseOver = false));
-        this.el.addEventListener("mousedown", () => (mouseDown = true));
-        this.el.addEventListener("mouseup", () => (mouseDown = false));
-      }
-
-      scaleImage(direction) {
-        const imageTl = gsap.timeline({
-          defaults: {
-            duration: 1.2,
-            ease: "elastic.out(1, 1)",
-            onUpdate: () => {
-              this.resize();
-            }
-          }
-        });
-        if (direction == "up") {
-          imageTl.to(this.el, {
-            scale: window.innerHeight / 600
-          });
-        } else if (direction == "down") {
-          imageTl.to(this.el, {
-            scale: 1
-          });
-        }
-      }
-
-      run() {
-        let m = mouseOver ? nMouse : new THREE.Vector2(0, 0);
-        this.mouse.lerp(m, this.mouseLerpAmount);
-
-        this.raycaster.setFromCamera(this.mouse, camera);
-        let intersects = this.raycaster.intersectObject(this.mesh);
-        if (intersects.length > 0) {
-          this.material.uniforms.uMousePos.value = [
-            intersects[0].uv.x,
-            intersects[0].uv.y
-          ];
-        }
-
-        if (mouseOver) {
-          this.material.uniforms.uMouseOverAmp.value = THREE.MathUtils.lerp(
-            this.material.uniforms.uMouseOverAmp.value,
-            1,
-            0.08
-          );
-          this.mouseLerpAmount = THREE.MathUtils.lerp(
-            this.mouseLerpAmount,
-            0.1,
-            0.5
-          );
-        } else {
-          this.material.uniforms.uMouseOverAmp.value = THREE.MathUtils.lerp(
-            this.material.uniforms.uMouseOverAmp.value,
-            0,
-            0.08
-          );
-          this.mouseLerpAmount = THREE.MathUtils.lerp(
-            this.mouseLerpAmount,
-            0,
-            0.5
-          );
-        }
-
-        if (mouseOver && mouseDown) {
-          this.material.uniforms.uRadius.value = THREE.MathUtils.lerp(
-            this.material.uniforms.uRadius.value,
-            1,
-            0.01
-          );
-        } else if (mouseOver && !mouseDown) {
-          this.material.uniforms.uRadius.value = THREE.MathUtils.lerp(
-            this.material.uniforms.uRadius.value,
-            0.08,
-            0.08
-          );
-        }
-
-        if (this.state.animating) {
-          this.material.uniforms.uMouseOverAmp.value = THREE.MathUtils.lerp(
-            this.material.uniforms.uMouseOverAmp.value,
-            0,
-            0.1
-          );
-        }
-      }
-    }
-
-    // -------- GL CLASSES [END] --------
 
     // -------- MAIN CLASSES [START] --------
 
---
+++ src/modules/my-stats/webGl/GlSlider.js
@@ -0,0 +1,247 @@
+import * as THREE from "three";
+import gsap from "gsap";
+import { initWebGl } from "@/modules/my-stats/webGl/webGl";
+
+let mouseOver = false,
+  mouseDown = false;
+let nMouse = new THREE.Vector2();
+
+export class GlSlider extends THREE.Object3D {
+  init(el) {
+    const vertexShader = document.getElementById("vertexShader").innerHTML;
+    const fragmentShader = document.getElementById("fragmentShader").innerHTML;
+
+    const planeGeometry = new THREE.PlaneBufferGeometry(1, 1, 32, 32);
+    const planeMaterial = new THREE.ShaderMaterial({
+      vertexShader,
+      fragmentShader
+    });
+
+    window.addEventListener("mousemove", event => {
+      event.preventDefault();
+      nMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      nMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    });
+
+    const { scene, camera } = initWebGl();
+    this.scene = scene;
+    this.camera = camera;
+
+    this.el = el;
+    this.resize();
+    this.geometry = planeGeometry;
+    this.material = planeMaterial.clone();
+
+    this.material.uniforms = {
+      uCurrTex: { value: 0 },
+      uNextTex: { value: 0 },
+      uTime: { value: 0 },
+      uProg: { value: 0 },
+      uAmplitude: { value: 0 },
+      uProgDirection: { value: 0 },
+      uMeshSize: { value: [this.rect.width, this.rect.height] },
+      uImageSize: { value: [0, 0] },
+      uMousePos: { value: [0, 0] },
+      uMouseOverAmp: { value: 0 },
+      uAnimating: { value: false },
+      uRadius: { value: 0.08 },
+      uTranslating: { value: true }
+    };
+
+    this.imageScale = 1;
+
+    this.textures = [];
+
+    this.raycaster = new THREE.Raycaster();
+    this.mouse = new THREE.Vector2();
+    this.mouseLerpAmount = 0.1;
+
+    this.state = {
+      animating: false,
+      current: 0
+    };
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.add(this.mesh);
+
+    this.scene.add(this);
+
+    this.loadTextures();
+    this.addEvents();
+  }
+
+  resize() {
+    this.setBounds();
+  }
+
+  setBounds() {
+    this.rect = this.el.getBoundingClientRect();
+
+    this.bounds = {
+      left: this.rect.left,
+      top: this.rect.top + window.scrollY,
+      width: this.rect.width,
+      height: this.rect.height
+    };
+
+    this.updateSize();
+    this.updatePosition();
+  }
+
+  updateSize() {
+    this.camUnit = this.calculateUnitSize(
+      this.camera.position.z - this.position.z
+    );
+
+    const x = this.bounds.width / window.innerWidth;
+    const y = this.bounds.height / window.innerHeight;
+
+    if (!x || !y) return;
+
+    this.scale.x = this.camUnit.width * x;
+    this.scale.y = this.camUnit.height * y;
+  }
+
+  calculateUnitSize(distance = this.position.z) {
+    const vFov = (this.camera.fov * Math.PI) / 180;
+    const height = 2 * Math.tan(vFov / 2) * distance;
+    const width = height * this.camera.aspect;
+
+    return { width, height };
+  }
+
+  updateY(y = 0) {
+    const { top, height } = this.bounds;
+
+    this.position.y = this.camUnit.height / 2 - this.scale.y / 2;
+    this.position.y -= ((top - y) / window.innerHeight) * this.camUnit.height;
+
+    this.progress = gsap.utils.clamp(
+      0,
+      1,
+      1 - (-y + top + height) / (window.innerHeight + height)
+    );
+  }
+
+  updateX(x = 0) {
+    const { left } = this.bounds;
+
+    this.position.x = -(this.camUnit.width / 2) + this.scale.x / 2;
+    this.position.x += ((left + x) / window.innerWidth) * this.camUnit.width;
+  }
+
+  updatePosition(y) {
+    this.updateY(y);
+    this.updateX(0);
+  }
+
+  loadTextures() {
+    const manager = new THREE.LoadingManager(() => {
+      this.material.uniforms.uCurrTex.value = this.textures[0];
+    });
+    const loader = new THREE.TextureLoader(manager);
+    const imgs = [...this.el.querySelectorAll("img")];
+
+    imgs.forEach(img => {
+      loader.load(img.src, texture => {
+        texture.minFilter = THREE.LinearFilter;
+        texture.generateMipmaps = false;
+
+        this.material.uniforms.uImageSize.value = [
+          img.naturalWidth,
+          img.naturalHeight
+        ];
+        this.textures.push(texture);
+      });
+    });
+  }
+
+  updateTime(time) {
+    this.material.uniforms.uTime.value = time;
+    this.run();
+  }
+
+  addEvents() {
+    this.el.addEventListener("mouseenter", () => (mouseOver = true));
+    this.el.addEventListener("mouseleave", () => (mouseOver = false));
+    this.el.addEventListener("mousedown", () => (mouseDown = true));
+    this.el.addEventListener("mouseup", () => (mouseDown = false));
+  }
+
+  scaleImage(direction) {
+    const imageTl = gsap.timeline({
+      defaults: {
+        duration: 1.2,
+        ease: "elastic.out(1, 1)",
+        onUpdate: () => {
+          this.resize();
+        }
+      }
+    });
+    if (direction === "up") {
+      imageTl.to(this.el, {
+        scale: window.innerHeight / 600
+      });
+    } else if (direction === "down") {
+      imageTl.to(this.el, {
+        scale: 1
+      });
+    }
+  }
+
+  run() {
+    let m = mouseOver ? nMouse : new THREE.Vector2(0, 0);
+    this.mouse.lerp(m, this.mouseLerpAmount);
+
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    let intersects = this.raycaster.intersectObject(this.mesh);
+    if (intersects.length > 0) {
+      this.material.uniforms.uMousePos.value = [
+        intersects[0].uv.x,
+        intersects[0].uv.y
+      ];
+    }
+
+    if (mouseOver) {
+      this.material.uniforms.uMouseOverAmp.value = THREE.MathUtils.lerp(
+        this.material.uniforms.uMouseOverAmp.value,
+        1,
+        0.08
+      );
+      this.mouseLerpAmount = THREE.MathUtils.lerp(
+        this.mouseLerpAmount,
+        0.1,
+        0.5
+      );
+    } else {
+      this.material.uniforms.uMouseOverAmp.value = THREE.MathUtils.lerp(
+        this.material.uniforms.uMouseOverAmp.value,
+        0,
+        0.08
+      );
+      this.mouseLerpAmount = THREE.MathUtils.lerp(this.mouseLerpAmount, 0, 0.5);
+    }
+
+    if (mouseOver && mouseDown) {
+      this.material.uniforms.uRadius.value = THREE.MathUtils.lerp(
+        this.material.uniforms.uRadius.value,
+        1,
+        0.01
+      );
+    } else if (mouseOver && !mouseDown) {
+      this.material.uniforms.uRadius.value = THREE.MathUtils.lerp(
+        this.material.uniforms.uRadius.value,
+        0.08,
+        0.08
+      );
+    }
+
+    if (this.state.animating) {
+      this.material.uniforms.uMouseOverAmp.value = THREE.MathUtils.lerp(
+        this.material.uniforms.uMouseOverAmp.value,
+        0,
+        0.1
+      );
+    }
+  }
+}
