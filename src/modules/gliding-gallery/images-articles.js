244670949fa3919a308ee4ccf797e19d2c37851d
Roland Doda <rolanddoda2014@gmail.com>
2020-12-16 17:26:37 +0100

    Download and use images for gliding gallery module

--- src/modules/gliding-gallery/images-articles.js
+++ src/modules/gliding-gallery/images-articles.js
@@ -1,41 +1,35 @@
 export default [
   {
-    src:
-      "https://www.thewowstyle.com/wp-content/uploads/2015/01/nature-images.jpg",
+    src: require("./images/image1.jpg"),
     title: "Stephen Shaw",
     description: "Lorem5"
   },
   {
-    src:
-      "https://www.wallpaperup.com/uploads/wallpapers/2016/03/29/918802/bdab201cf97ede07f0bf35e1f545012b.jpg",
+    src: require("./images/image2.jpg"),
     title: "Keyframes",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque iure nam quam quasi reprehenderit."
   },
   {
-    src:
-      "https://thewallpaper.co//wp-content/uploads/2017/09/wallpaper-mobile-background-nature-cool-images-fog-mother-nature-lake-ultrahd-colourful-forest-mountain-k-landscape.jpg",
+    src: require("./images/image3.jpg"),
     title: "David Khourshid",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque iure nam quam quasi reprehenderit."
   },
   {
-    src:
-      "https://www.jakpost.travel/wimages/large/48-482688_blue-sunset-wallpaper-png-hd-background-nature.jpg",
+    src: require("./images/image4.jpg"),
     title: "Roland Doda",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque iure nam quam quasi reprehenderit."
   },
   {
-    src:
-      "https://i0.wp.com/mojly.com/wp-content/uploads/2018/03/nature-amazing-pics-hd-wallpapers-desktop-backgrounds-images-beach-grass-against-the-winter-sunset-on-lake-michigan-wallpaper.jpg?fit=2560%2C1440&ssl=1",
+    src: require("./images/image5.jpg"),
     title: "Mariglen Doda",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque iure nam quam quasi reprehenderit."
   },
   {
-    src:
-      "https://wallpapertag.com/wallpaper/full/5/c/7/125476-background-nature-2560x1600-720p.jpg",
+    src: require("./images/image6.jpg"),
     title: "Klodian shaba",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque iure nam quam quasi reprehenderit."
