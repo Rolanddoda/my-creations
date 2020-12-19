79bc9bad8881b5c12c7913ccc66384b5a790aa69
Roland Doda <rolanddoda2014@gmail.com>
2020-12-19 20:52:07 +0100

    Add glass-morphism component + other things

---
+++ src/modules/my-stats/components/stats-component/glass-morphism/CardEffect.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="glass-morphism-effect" :style="{ '--blur': blur + 'px' }"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    blur: {
+      type: Number,
+      default: 4
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.glass-morphism-effect {
+  position: absolute;
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(var(--blur));
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+  animation: fly 2s ease-in-out alternate infinite;
+}
+
+@keyframes fly {
+  from {
+    transform: translateY(-10px);
+  }
+  to {
+    transform: translateY(10px);
+  }
+}
+</style>
---
+++ src/modules/my-stats/components/stats-component/glass-morphism/Wrapper.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    class="wrapper"
+    :style="{
+      '--top': top !== undefined ? top + 'px' : 'unset',
+      '--right': right !== undefined ? right + 'px' : 'unset',
+      '--bottom': bottom !== undefined ? bottom + 'px' : 'unset',
+      '--left': left !== undefined ? left + 'px' : 'unset'
+    }"
+  >
+    <div class="text"><slot>Put text here</slot></div>
+    <CardEffect class="card-effect-1" :blur="1" />
+    <CardEffect class="card-effect-2" />
+    <CardEffect class="card-effect-3" />
+  </div>
+</template>
+
+<script>
+import CardEffect from "./CardEffect";
+
+export default {
+  components: {
+    CardEffect
+  },
+
+  props: {
+    top: Number,
+    right: Number,
+    bottom: Number,
+    left: Number
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Leckerli+One&display=swap");
+
+.wrapper {
+  position: absolute;
+  top: var(--top);
+  right: var(--right);
+  bottom: var(--bottom);
+  left: var(--left);
+
+  .text {
+    font-family: "Leckerli One", cursive;
+    font-size: 10vmin;
+  }
+
+  .card-effect-1 {
+    width: 60%;
+    height: 100%;
+    top: 0;
+    left: 20%;
+  }
+
+  .card-effect-2 {
+    width: 50px;
+    height: 50px;
+    top: -20px;
+    left: 25%;
+  }
+
+  .card-effect-3 {
+    width: 80px;
+    height: 80px;
+    bottom: -40px;
+    right: 25%;
+  }
+}
+</style>
